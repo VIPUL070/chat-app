fa13a47e4fc92bb795a317ee9c7cce7ee32cd9c2
VIPUL070 <sutharvipul007@gmail.com>
2026-04-18 19:48:28 +0530

    building minimal frontend of the chat app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+  
+  return (
+    <div className="h-dvh bg-black pl-20 pr-20">
+      <div className="h-[90dvh]"></div>
+      <div className="flex gap-4 items-center justify-center">
+        <input
+          type="text"
+          className="w-[60vw] border border-offwhite rounded-md p-3 
+             outline-none transition-all
+             focus:border-yellow-light focus:ring-1 focus:ring-yellow-light"
+          placeholder="Modern input..."
+        />
+        <button className="bg-offwhite p-3 w-[10vw] text-xl font-bold rounded-md cursor-pointer hover:bg-yellow-light transition-all duration-600">
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
